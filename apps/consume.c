5d730d3ffb0be899ded7ae4c7f6b51e716d806f7
Dominik Kovacs <dkovacs@iu.edu>
2022-01-18 15:54:39 -0500

    Update skeleton code for Assignment 2

--- apps/consume.c
+++ apps/consume.c
@@ -1,6 +1,8 @@
 #include <xinu.h>
-//#include <prodcons.h>
+#include <prodcons.h>
 
 void consumer(int count) {
+  // TODO: implement the following:
+  // reads the value of the global variable 'n' 'count' times
+  // print consumed value (the value of 'n'): "consumed : 8"
 }
-
--- apps/produce.c
+++ apps/produce.c
@@ -1,8 +1,8 @@
 #include <xinu.h>
-//#include <prodcons.h>
+#include <prodcons.h>
 
 void producer(int count) {
+  // TODO: implement the following:
+  // Iterates from 0 to count, setting the value of the global variable 'n' each time
+  // print produced value (new value of 'n'): "produced : 8"
 }
-
-
-
