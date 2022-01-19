198aa7c6b37ce9af3147853ecdac85a4b4827ba7
jagchau <jagchau@iu.edu>
2022-01-19 16:08:31 -0500

    Updated xsh_hello.c

--- shell/xsh_hello.c
+++ shell/xsh_hello.c
@@ -23,21 +23,21 @@ shellcmd xsh_hello(int nargs, char *args[]) {
 
 	/* Check argument count */
 
-	if (nargs > 1) {
+	if (nargs > 2) {
 		fprintf(stderr, "%s: too many arguments\n", args[0]);
 		fprintf(stderr, "Try '%s --help' for more information\n",
 			args[0]);
 		return 1;
 	}
-	if (nargs < 1) {
+	if (nargs < 2) {
 		fprintf(stderr, "%s: too less arguments\n", args[0]);
 		fprintf(stderr, "Try '%s --help' for more information\n",
 			args[0]);
 		return 1;
 	}
 
-	if (nargs == 1) {
-		fprintf("Hello %s, Welcome to the world of Xinu!!", args[0]);
+	if (nargs == 2) {
+		printf("Hello %s, Welcome to the world of Xinu!!\n", args[1]);
 	}
 
 	return 0;
